424e031917abfa21add091db53e7fb09a9e43101
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-08-07 13:48:05 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -6,7 +6,7 @@ import style from './ContactForm.module.css';
 export class ContactForm extends Component {
   state = {
     name: '',
-    number: '',
+    number: 0,
   };
 
   handleChange = e => {
@@ -41,9 +41,9 @@ export class ContactForm extends Component {
           type="text"
           name="name"
           // оригинальный паттерн из-за которого ошибка в консоли
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           // новый исправленный паттерн \\
-          // pattern="^[a-zA-Zа-яА-Я]+(['\- ][a-zA-Zа-яА-Я ]*[a-zA-Zа-яА-Я])?$"
+          pattern="^[a-zA-Zа-яА-Я]+(['\- ][a-zA-Zа-яА-Я ]*[a-zA-Zа-яА-Я])?$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           onChange={this.handleChange}
@@ -55,7 +55,9 @@ export class ContactForm extends Component {
           type="tel"
           name="number"
           // оригинальный паттерн из-за которого ошибка в консоли
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          // новый исправленный паттерн \\
+          pattern="\+?\d{1,4}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           onChange={this.handleChange}
